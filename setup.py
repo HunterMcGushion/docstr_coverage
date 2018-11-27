60430174ffd809f27a37bc1e1160e5d56ec20b82
Hunter McGushion <hunter@mcgushion.com>
2018-11-27 15:27:40 -0800

    Make setup black-compliant

--- setup.py
+++ setup.py
@@ -2,49 +2,47 @@ from setuptools import setup, find_packages
 
 
 def readme():
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
 
 MAJOR, MINOR, MICRO = 1, 0, 1
-__VERSION__ = '{}.{}.{}'.format(MAJOR, MINOR, MICRO)
+__VERSION__ = "{}.{}.{}".format(MAJOR, MINOR, MICRO)
 
 setup(
-    name='docstr_coverage',
+    name="docstr_coverage",
     version=__VERSION__,
-    description=' '.join([
-        'Utility for examining python source files to ensure proper documentation.',
-        'Lists missing docstrings, and calculates overall docstring coverage percentage rating'
-    ]),
+    description=" ".join(
+        [
+            "Utility for examining python source files to ensure proper documentation.",
+            "Lists missing docstrings, and calculates overall docstring coverage percentage rating",
+        ]
+    ),
     long_description=readme(),
-    long_description_content_type='text/markdown',
-    keywords='docstring coverage documentation audit source code statistics report',
-    url='https://github.com/HunterMcGushion/docstr_coverage',
-    author='Hunter McGushion',
-    author_email='hunter@mcgushion.com',
-    license='MIT',
+    long_description_content_type="text/markdown",
+    keywords="docstring coverage documentation audit source code statistics report",
+    url="https://github.com/HunterMcGushion/docstr_coverage",
+    author="Hunter McGushion",
+    author_email="hunter@mcgushion.com",
+    license="MIT",
     packages=find_packages(),
     install_requires=[],
     include_package_data=True,
     zip_safe=False,
-    test_suite='nose.collector',
-    tests_require=['nose'],
-    entry_points=dict(
-        console_scripts=[
-            'docstr-coverage=docstr_coverage.coverage:_execute',
-        ],
-    ),
+    test_suite="nose.collector",
+    tests_require=["nose"],
+    entry_points=dict(console_scripts=["docstr-coverage=docstr_coverage.coverage:_execute"]),
     classifiers=(
         # TODO: Check Python 2 compatibility
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Topic :: Documentation',
-        'Topic :: Documentation :: Sphinx',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Documentation',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Software Development :: Quality Assurance',
-        'Topic :: Utilities'
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Documentation",
+        "Topic :: Documentation :: Sphinx",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Utilities",
     ),
 )
