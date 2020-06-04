b05c63c1f6db27c24c759d2697787036f1c4a303
Hunter McGushion <hunter@mcgushion.com>
2020-06-04 14:16:51 -0700

    Fix `-d` option breaking verbose level 3 (#19)
    Fix `-d` option breaking verbose level 3. Closes #8
    
    Use `logging` module instead of `print` statements
    This change was mostly made to simplify unit testing for reports
    
    Convert f-string to standard string formatting for Py3.5 compatibility
    
    Add regression tests for #8 fix
    
    Update changelog for #19
    
    Address Flake8 line length complaints

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,17 +2,19 @@
 ## [Unreleased]
 
 ### Bug Fixes
-* Fixed bug where a total coverage of 0 would break everything ([killthekitten])
+* Fixed bug where a total coverage of 0 would break everything. [#16] by [killthekitten].
+* Fixed bug where the `-d`/`--docstr-ignore-file` CLI option to provide an `ignore_names_file` 
+  would not work with `-v 3`/`--verbose=3`. [#19] by [HunterMcGushion].
 
 ### Changes
-* Addition of testing safety net ([killthekitten])
+* Addition of testing safety net. [#16] by [killthekitten].
 
 
 <a name="1.1.0"></a>
 ## [1.1.0] (2020-05-20)
 
 ### Features
-* Added `--failunder`/`-F` option to fail if coverage is below a given percentage ([econchick])
+* Added `--failunder`/`-F` option to fail if coverage is below a given percentage. [#11] by [econchick].
     * Default=100.0
 
 
@@ -21,22 +23,22 @@
 
 ### Features
 * Added `--docstr-ignore-file`/`-d` option to provide a file, containing a list of patterns to 
-  ignore during coverage calculation ([Redysz])
-* Added support for raising the exit code to be used with CI pipelines ([sim0nx])
+  ignore during coverage calculation. [#4] by [Redysz].
+* Added support for raising the exit code to be used with CI pipelines. [#6] by [sim0nx].
 
 
 <a name="1.0.4"></a>
 ## [1.0.4] (2019-04-17)
 
 ### Features
-* Added support for non-ASCII characters ([Redysz])
+* Added support for non-ASCII characters. [#3] by [Redysz].
 
 
 <a name="1.0.3"></a>
 ## [1.0.3] (2019-01-28)
 
 ### Bug Fixes
-* Fixed bug preventing `docstr-coverage` from being properly installed via tox ([asergeant01])
+* Fixed bug preventing `docstr-coverage` from being properly installed via tox. [#2] by [asergeant01].
 
 
 <a name="0.1.0"></a>
@@ -46,14 +48,25 @@
 * Initial release
 
 
-[asergeant01]: https://github.com/asergeant01
-[econchick]: https://github.com/econchick
-[killthekitten]: https://github.com/killthekitten
-[Redysz]: https://github.com/Redysz
-[sim0nx]: https://github.com/sim0nx
-
 [Unreleased]: https://github.com/HunterMcGushion/docstr_coverage/compare/v1.1.0...HEAD
 [1.1.0]: https://github.com/HunterMcGushion/docstr_coverage/compare/v1.0.5...v1.1.0
 [1.0.5]: https://github.com/HunterMcGushion/docstr_coverage/compare/v1.0.4...v1.0.5
 [1.0.4]: https://github.com/HunterMcGushion/docstr_coverage/compare/v1.0.3...v1.0.4
 [1.0.3]: https://github.com/HunterMcGushion/docstr_coverage/compare/v1.0.2...v1.0.3
+
+
+[asergeant01]: https://github.com/asergeant01
+[econchick]: https://github.com/econchick
+[HunterMcGushion]: https://github.com/HunterMcGushion
+[killthekitten]: https://github.com/killthekitten
+[Redysz]: https://github.com/Redysz
+[sim0nx]: https://github.com/sim0nx
+
+
+[#2]: https://github.com/HunterMcGushion/docstr_coverage/pull/2
+[#3]: https://github.com/HunterMcGushion/docstr_coverage/pull/3
+[#4]: https://github.com/HunterMcGushion/docstr_coverage/pull/4
+[#6]: https://github.com/HunterMcGushion/docstr_coverage/pull/6
+[#11]: https://github.com/HunterMcGushion/docstr_coverage/pull/11
+[#16]: https://github.com/HunterMcGushion/docstr_coverage/pull/16
+[#19]: https://github.com/HunterMcGushion/docstr_coverage/pull/19
--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -1,6 +1,9 @@
+import logging
 import os
+import pytest
 
 from docstr_coverage import get_docstring_coverage
+from docstr_coverage.coverage import GRADES
 
 SAMPLES_DIRECTORY = os.path.join("tests", "sample_files")
 EMPTY_FILE_PATH = os.path.join(SAMPLES_DIRECTORY, "empty_file.py")
@@ -10,7 +13,7 @@ SOME_CODE_NO_DOCS_FILE_PATH = os.path.join(SAMPLES_DIRECTORY, "some_code_no_docs
 
 
 def test_should_report_for_an_empty_file():
-    file_results, total_results = get_docstring_coverage([EMPTY_FILE_PATH],)
+    file_results, total_results = get_docstring_coverage([EMPTY_FILE_PATH])
     assert file_results == {
         EMPTY_FILE_PATH: {
             "missing": [],
@@ -25,7 +28,7 @@ def test_should_report_for_an_empty_file():
 
 
 def test_should_report_full_coverage():
-    file_results, total_results = get_docstring_coverage([DOCUMENTED_FILE_PATH],)
+    file_results, total_results = get_docstring_coverage([DOCUMENTED_FILE_PATH])
     assert file_results == {
         DOCUMENTED_FILE_PATH: {
             "missing": [],
@@ -105,3 +108,104 @@ def test_should_report_when_no_docs_in_a_file():
         }
     }
     assert total_results == {"missing_count": 2, "needed_count": 2, "coverage": 0.0}
+
+
+##################################################
+# Logging Tests
+##################################################
+@pytest.mark.parametrize(
+    ["expected"],
+    [
+        (
+            [
+                '\nFile: "tests/sample_files/empty_file.py"',
+                " - File is empty",
+                " Needed: 0; Found: 0; Missing: 0; Coverage: 0.0%",
+                "\n",
+                "Overall statistics (1 files are empty):",
+                "Needed: 0  -  Found: 0  -  Missing: 0",
+                "Total coverage: 100.0%  -  Grade: " + GRADES[0][0],
+            ],
+        )
+    ],
+)
+def test_logging_empty_file(caplog, expected):
+    with caplog.at_level(logging.DEBUG):
+        _file_results, _total_results = get_docstring_coverage([EMPTY_FILE_PATH], verbose=3)
+
+    assert caplog.messages == expected
+
+
+@pytest.mark.parametrize(
+    ["expected", "verbose", "ignore_names"],
+    [
+        (
+            [
+                '\nFile: "tests/sample_files/partly_documented_file.py"',
+                " - No module docstring",
+                " - No docstring for `foo`",
+                " - No docstring for `bar`",
+                " Needed: 4; Found: 1; Missing: 3; Coverage: 25.0%",
+                "\n",
+                "Overall statistics:",
+                "Needed: 4  -  Found: 1  -  Missing: 3",
+                "Total coverage: 25.0%  -  Grade: " + GRADES[6][0],
+            ],
+            3,
+            ([".*", "__.+__"],),
+        ),
+        (
+            [
+                '\nFile: "tests/sample_files/partly_documented_file.py"',
+                " - No module docstring",
+                " - No docstring for `FooBar.__init__`",
+                " - No docstring for `foo`",
+                " - No docstring for `bar`",
+                " Needed: 5; Found: 1; Missing: 4; Coverage: 20.0%",
+                "\n",
+                "Overall statistics:",
+                "Needed: 5  -  Found: 1  -  Missing: 4",
+                "Total coverage: 20.0%  -  Grade: " + GRADES[7][0],
+            ],
+            3,
+            (),
+        ),
+        (
+            [
+                '\nFile: "tests/sample_files/partly_documented_file.py"',
+                " Needed: 5; Found: 1; Missing: 4; Coverage: 20.0%",
+                "\n",
+                "Overall statistics:",
+                "Needed: 5  -  Found: 1  -  Missing: 4",
+                "Total coverage: 20.0%  -  Grade: " + GRADES[7][0],
+            ],
+            2,
+            (),
+        ),
+        (
+            [
+                "Overall statistics:",
+                "Needed: 5  -  Found: 1  -  Missing: 4",
+                "Total coverage: 20.0%  -  Grade: " + GRADES[7][0],
+            ],
+            1,
+            (),
+        ),
+        (
+            [
+                "Overall statistics:",
+                "Needed: 2  -  Found: 1  -  Missing: 1",
+                "Total coverage: 50.0%  -  Grade: " + GRADES[5][0],
+            ],
+            1,
+            ([".*", ".*"],),
+        ),
+    ],
+)
+def test_logging_partially_documented_file(caplog, expected, verbose, ignore_names):
+    with caplog.at_level(logging.DEBUG):
+        _file_results, _total_results = get_docstring_coverage(
+            [PARTLY_DOCUMENTED_FILE_PATH], verbose=verbose, ignore_names=ignore_names
+        )
+
+    assert caplog.messages == expected
