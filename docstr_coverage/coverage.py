a9ad13d0f231548fac10279e0db2a0466ee78893
Hunter McGushion <hunter@mcgushion.com>
2020-06-16 11:30:19 -0700

    Add `visitor` module

--- docstr_coverage/coverage.py
+++ docstr_coverage/coverage.py
@@ -2,7 +2,7 @@
 and James Harlow (see "THANKS.txt" for details)"""
 
 # TODO: If Python 2, ```from __future__ import print_function```
-from ast import NodeVisitor, parse, get_docstring
+from ast import parse
 import logging
 import os
 from pathlib import Path
@@ -11,50 +11,10 @@ import sys
 from typing import List, Tuple
 
 from docstr_coverage.badge import Badge
+from docstr_coverage.visitor import DocStringCoverageVisitor
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="%(message)s")
-
-
-class DocStringCoverageVisitor(NodeVisitor):
-    """Class to visit nodes, determine whether a node requires a docstring,
-    and to check for the existence of a docstring"""
-
-    def __init__(self):
-        self.symbol_count = 0
-        self.tree = []
-
-    def visit_Module(self, node):
-        """Upon visiting a module, initialize :attr:`DocStringCoverageVisitor.tree`
-        with module-wide node info."""
-
-        has_doc = get_docstring(node) is not None and get_docstring(node).strip() != ""
-        is_empty = not len(node.body)
-        self.tree.append((has_doc, is_empty, []))
-        self.generic_visit(node)
-
-    def visit_ClassDef(self, node):
-        """Collect information regarding class declaration nodes"""
-
-        self._visit_helper(node)
-
-    def visit_FunctionDef(self, node):
-        """Collect information regarding function/method declaration nodes"""
-
-        self._visit_helper(node)
-
-    def _visit_helper(self, node):
-        """Helper method to update :attr:`DocStringCoverageVisitor.tree` with pertinent
-        documentation information for `node`, then ensure all child nodes are
-        also visited"""
-
-        self.symbol_count += 1
-        has_doc = get_docstring(node) is not None and get_docstring(node).strip() != ""
-        _node = (node.name, has_doc, [])
-        self.tree[-1][-1].append(_node)
-        self.tree.append(_node)
-        self.generic_visit(node)
-        self.tree.pop()
 
 
 GRADES = (
