004c72a6002f1be2b4f353ca178a01a9bf4aaa88
Michael Weiss <code@mweiss.ch>
2021-04-18 17:18:11 +0200

    Expose

--- docstr_coverage/__init__.py
+++ docstr_coverage/__init__.py
@@ -2,5 +2,17 @@
 # Set __all__
 ##################################################
 from .coverage import analyze, get_docstring_coverage
+from .ignore_config import IgnoreConfig
+from .printers import LegacyPrinter
+from .result_collection import ResultCollection, AggregatedCount, ExpectedDocstring, FileCount
 
-__all__ = ["analyze", "get_docstring_coverage"]
+__all__ = [
+    "analyze",
+    "get_docstring_coverage",
+    "IgnoreConfig",
+    "LegacyPrinter",
+    "ResultCollection",
+    "AggregatedCount",
+    "ExpectedDocstring",
+    "FileCount",
+]
