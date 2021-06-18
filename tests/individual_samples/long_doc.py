66c42c15e05a146dbedda829cab7e61facda654e
Michael Weiss <code@mweiss.ch>
2021-06-18 20:44:27 +0200

    fix #79 and add test

---
+++ tests/individual_samples/long_doc.py
@@ -0,0 +1,23 @@
+"""
+this is a very long docstring
+
+this is a very long docstring
+this is a very long docstring
+this is a very long docstring
+this is a very long docstring
+this is a very long docstring
+this is a very long docstring
+this is a very long docstring
+this is a very long docstring
+"""
+
+
+class A:
+    """This is the first class in the alphabeth."""
+
+    # docstr-coverage:excused `test ignore after long docstrings`
+    def ignored(self):
+        pass
+
+    def missing(self):
+        pass
