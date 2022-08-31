f78572586104a5ccaa24b8839d400444b0ec7fda
Michael <code@mweiss.ch>
2022-08-31 08:41:40 +0200

    Linting

--- docstr_coverage/cli.py
+++ docstr_coverage/cli.py
@@ -46,7 +46,7 @@ def do_include_filepath(filepath: str, exclude_re: Optional["re.Pattern"]) -> bo
 
 
 def collect_filepaths(
-        *paths: str, follow_links: bool = False, exclude: Optional[str] = None
+    *paths: str, follow_links: bool = False, exclude: Optional[str] = None
 ) -> List[str]:
     """Collect filepaths under given `paths` that are not `exclude`-d
 
@@ -379,8 +379,9 @@ def _deprecation_alerts(kwargs):
             return False
         return os.path.split(path)[-1] != ".docstr_coverage" or os.path.isfile(path)
 
-    if (_nondefault_or_existing_file(ignore_file_old_casing)
-            or _nondefault_or_existing_file(ignore_file_new_casing)):
+    if _nondefault_or_existing_file(ignore_file_old_casing) or _nondefault_or_existing_file(
+        ignore_file_new_casing
+    ):
         click.secho(
             "Using deprecated ignore files."
             "We'll keep them supported for a while, "
