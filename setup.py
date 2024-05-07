d4ae6d6d6e974ea76f23fd9de8111716b771210b
Michael Weiss <code@mweiss.ch>
2024-05-07 07:30:47 +0200

    tie tqdm version less restrictively

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "click",
         "PyYAML",
-        "tqdm==4.63.1",
+        "tqdm~~4.0",
         "importlib_resources; python_version < '3.9'",
     ],
     extras_require={
