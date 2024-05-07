8ae362de940985366bb1c1cae1d0d8ce57b4700a
Michael Weiss <code@mweiss.ch>
2024-05-07 07:32:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "click",
         "PyYAML",
-        "tqdm~~4.0",
+        "tqdm~4.0",
         "importlib_resources; python_version < '3.9'",
     ],
     extras_require={
