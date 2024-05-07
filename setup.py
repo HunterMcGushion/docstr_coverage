a6454c8b95bba7526cef59bda29d5be2e60cbd07
Michael Weiss <code@mweiss.ch>
2024-05-07 18:19:34 +0200

    :bug: tie tqdm version less restrictively (#142)
    * tie tqdm version less restrictively
    
    * Update setup.py
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "click",
         "PyYAML",
-        "tqdm==4.63.1",
+        "tqdm",
         "importlib_resources; python_version < '3.9'",
     ],
     extras_require={
